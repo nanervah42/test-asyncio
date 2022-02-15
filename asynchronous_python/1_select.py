cf6baf296557660870f92abafd3c16b91ff3b977
nanervah <nanervah@yandex.ru>
2022-02-15 18:00:02 +0700

    #4

---
+++ asynchronous_python/1_select.py
@@ -0,0 +1,34 @@
+import socket
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('localhost', 5000))
+server_socket.listen()
+
+def accept_connection(server_socket):
+    while True:
+        if server_socket == True:
+            print('yes')
+        else:
+            print('no')
+        client_socket, addr = server_socket.accept()
+        print('Connection from', addr)
+
+
+
+def send_message(client_socket):
+    while True:
+        request = client_socket.recv(4096)
+
+        if not request:
+            break
+        else:
+            response = 'Hello world\n'.encode()
+            client_socket.send(response)
+
+    client_socket.close()
+
+
+if __name__ == '__main__':
+    accept_connection(server_socket)
--- temp1.py
+++ temp1.py
@@ -1,9 +1,1 @@
-import requests
-import time
-
-begin = time.time()
-
-for i in range(10):
-    req = requests.get('https://api.covidtracking.com/v1/us/current.json')
-
-print(time.time() - begin)
+import socket
